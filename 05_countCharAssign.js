33f94f9d0c6f92bf9f4bf24e4b19548d57c82bca
7773943421 <ashitoshchoudhari19@gmail.com>
2022-12-22 20:39:49 +0530

    Update_05_countCharAssign.js
    Update_05_countCharAssign.js

--- 05_countCharAssign.js
+++ 05_countCharAssign.js
@@ -1,33 +1,17 @@
 
-
-var sentence = "I AM Learning ,the javaScript,The language of internte";
-// Print the vowels only aeiou or AEIOU
-var counter = 0;
-for (let index = 0; index < sentence.length; index++) {
-    var char = sentence.charAt(index);
-    if (char=='a' ||   char=='A')  {
+function counterCharA(string) {
+    var counter = 0;
+    for (let index = 0; index < string.length; index++) {
+      var char = string.charAt(index);
+      if (char == "a" || char == "A") {
         console.log(char);
-        counter = counter +1;
-    }   
-}
-console.log("I AM Learning ,the javaScript,The language of internte");
-console.log(`Total no of a and A is : ${counter}`);
-
-
-
-var sentence = "My favourite movi is LAggAn";
-// Print the vowels only aeiou or AEIOU
-var counter = 0;
-for (let index = 0; index < sentence.length; index++) {
-    var char = sentence.charAt(index);
-    if (char=='a' ||   char=='A')  {
-        console.log(char);
-        counter = counter +1;
-    }   
-}
-console.log("My favourite movi is LAggAn");
-console.log(`Total no of a and A is : ${counter}`);
-
-
-
-function count
+        counter = counter + 1;
+      }
+    }
+    console.log(`${string}${counter}`);
+  }
+  counterCharA(`I AM Learning JavaScript, The Language of internet :- `);
+  
+  console.log("---------------------------------------------------------------");
+  
+  counterCharA(`My favourite movie is LAggAn :- `);
